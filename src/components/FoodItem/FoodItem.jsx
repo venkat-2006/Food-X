d6e67ce7e454b07aa588de13f5e4336302ffbd6a
VENKAT6912 <vibecoder18@gmail.com>
2025-09-13 02:37:01 +0530

    added + and - for items

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -3,10 +3,19 @@ import './FoodItem.css'
 import { assets } from '../../assets/assets'
 
 const FoodItem = ({ id, name, price, description, image }) => {
+    const[itemCount,setItemCount] = React.useState(0);
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img src={image} alt="" className="food-item-image" />
+                {!itemCount
+                    ?<img src={assets.add_icon_white} alt="" className="add" onClick={()=>setItemCount(prev=>prev+1)}/>
+                    :<div className="food-item-counter">
+                        <img src={assets.remove_icon_red} alt="" onClick={()=>setItemCount(prev=>prev-1)}/>
+                        <p>{itemCount}</p>
+                        <img src={assets.add_icon_green} alt="" onClick={()=>setItemCount(prev=>prev+1)}/>
+                        </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
