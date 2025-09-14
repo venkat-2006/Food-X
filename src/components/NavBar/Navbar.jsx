4ffb65b07d11238691bb6aee2643b8f52400fe1c
VENKAT6912 <vibecoder18@gmail.com>
2025-09-15 01:49:28 +0530

    cart skeleton and link using path

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({setShowLoginPopup}) => {
     const[menu,setMenu]=React.useState("home"); 
     return (
         <div className="navbar">
-            <img src={assets.logo} alt="" className="logo" />
+            <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
             <ul className="navbar-menu">
                 <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
                 <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
@@ -17,7 +17,7 @@ const Navbar = ({setShowLoginPopup}) => {
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="" />
+                    <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                     <div className="dot"></div>
                 </div>
                 <button onClick={()=>setShowLoginPopup(true)}>Sign in</button>
