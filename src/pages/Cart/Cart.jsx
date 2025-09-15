419ab2e8c0ad0c424a9f153ff42817ac2ab8cb8d
VENKAT6912 <vibecoder18@gmail.com>
2025-09-15 13:06:16 +0530

    fixed cart logic at payment

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -56,12 +56,12 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2 }</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
           <button onClick={()=>navigate('/order')}>Proceed to checkout</button>
